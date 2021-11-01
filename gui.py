4630b95cd4f9dcd5b12a74fee53847f2e997fea5
murdoch <murdoch@Murdochs-MacBook-Air.local>
2021-10-31 21:48:56 -0400

    Sped up startup speed.

--- gui.py
+++ gui.py
@@ -20,13 +20,14 @@ class ItemSpriteHandler:
         self.images = []
         for row in range(self.spritesheet_height):
             for col in range(self.spritesheet_width):
-                l = col * self.sprite_width
-                t = row * self.sprite_height
-                self.images.append(
-                    self.subimage(
-                        l, t, l + self.sprite_width, t + self.sprite_height
-                    )
-                )
+                self.images.append(None)
+                # l = col * self.sprite_width
+                # t = row * self.sprite_height
+                # self.images.append(
+                # self.subimage(
+                # l, t, l + self.sprite_width, t + self.sprite_height
+                # )
+                # )
 
         # Load the dictionary that will map from the minecraft id string to
         # the index in the images list
@@ -39,18 +40,33 @@ class ItemSpriteHandler:
     def subimage(self, l, t, r, b):
         new_image = tk.PhotoImage()
         new_image.tk.call(
-            new_image, 
-            'copy', 
-            self.spritesheet, 
-            '-from', 
-            l, t, r, b, 
-            '-to', 
+            new_image,
+            'copy',
+            self.spritesheet,
+            '-from',
+            l, t, r, b,
+            '-to',
             0, 0
         )
         return new_image
 
+    # def get_image(self, minecraft_id: str) -> tk.PhotoImage:
+        # return self.images[self.mcid_to_index[minecraft_id]]
+
     def get_image(self, minecraft_id: str) -> tk.PhotoImage:
-        return self.images[self.mcid_to_index[minecraft_id]]
+        img = self.images[self.mcid_to_index[minecraft_id]]
+        if img is None:
+            index = self.mcid_to_index[minecraft_id]
+            row = index // self.spritesheet_height
+            col = index % self.spritesheet_width
+            l = col * self.sprite_width
+            t = row * self.sprite_height
+            self.images[index] = self.subimage(
+                l, t, l + self.sprite_width, t + self.sprite_height
+            )
+            return self.images[index]
+        else:
+            return img
 
 
 class ModifyItemPopupWindow:
@@ -59,7 +75,7 @@ class ModifyItemPopupWindow:
         window = tk.Toplevel(master)
         self.window = window
 
-        # Variables we'll use 
+        # Variables we'll use
         self.var_slot = tk.IntVar(window, 0, name='Slot')
         self.var_count = tk.IntVar(window, 0, name='Count')
         self.var_id = tk.StringVar(window, '', name='ID')
@@ -77,8 +93,8 @@ class ModifyItemPopupWindow:
         # Create the GUI representation for each of the variables
         slotLabel = tk.Label(window, text="Slot: ")
         slotLabel.pack(fill='x', padx=50, pady=5)
-        slotEntry = tk.Entry(window, state=DISABLED, 
-                            textvariable=self.var_slot)
+        slotEntry = tk.Entry(window, state=DISABLED,
+                             textvariable=self.var_slot)
         slotEntry.pack(fill='x', padx=50, pady=5)
 
         countLabel = tk.Label(window, text="Count: ")
@@ -125,14 +141,14 @@ class ModifyItemPopupWindow:
 
 class ItemSlot(tk.Frame):
     def __init__(self, master, row, column, slot_num):
-        super().__init__(master, 
-                        # bg="#898c87", 
-                        bg="#a6a6a6",
-                        width=64, 
-                        height=64)
+        super().__init__(master,
+                         # bg="#898c87",
+                         bg="#a6a6a6",
+                         width=64,
+                         height=64)
         self.grid(column=column, row=row, padx=5, pady=5)
         self.popup_window = None
-        
+
         # New item object that will store the item data associated with the
         # slot
         self.item = Item(slot=slot_num)
@@ -147,15 +163,16 @@ class ItemSlot(tk.Frame):
 
         self.image_label.bind('<Button-1>', self.handle_click)
 
-        self.count_label = tk.Label(self, bg="#a6a6a6", text="", borderwidth=1, relief='solid')
-        self.count_label.place(relx=1.0, rely=1.0, x=-2, y=-2,anchor="se")
+        self.count_label = tk.Label(
+            self, bg="#a6a6a6", text="", borderwidth=1, relief='solid')
+        self.count_label.place(relx=1.0, rely=1.0, x=-2, y=-2, anchor="se")
         self.count_label.place_forget()
-    
+
     def handle_click(self, event):
         self.popup_window = ModifyItemPopupWindow(
-            None, 
-            self, 
-            self.item, 
+            None,
+            self,
+            self.item,
             # self.on_save
             self.update_display
         )
@@ -165,14 +182,14 @@ class ItemSlot(tk.Frame):
     def update_display(self):
         global itemSpriteHandler
         if self.item.is_valid():
-            self.image_label.configure(image=itemSpriteHandler.get_image(self.item.id))
+            self.image_label.configure(
+                image=itemSpriteHandler.get_image(self.item.id))
             self.count_label.configure(text=str(self.item.count))
-            self.count_label.place(relx=1.0, rely=1.0, x=-2, y=-2,anchor="se")
+            self.count_label.place(relx=1.0, rely=1.0, x=-2, y=-2, anchor="se")
         else:
             self.image_label.configure(image=None)
             self.count_label.configure(text="")
             self.count_label.place_forget()
-
 
     def set_item(self, item: Item):
         self.item = item
@@ -197,14 +214,14 @@ class App(tk.Frame):
         # Add all of the item slots
         self.itemslots = {}
 
-        self.itemSpriteHandler = ItemSpriteHandler()
+        #self.itemSpriteHandler = ItemSpriteHandler()
 
         # Add the armour slots
         slot_num = 103
         for r in range(0, 4):
             self.itemslots[slot_num] = ItemSlot(self, r, 0, slot_num)
             slot_num -= 1
-        
+
         # Add the shield slot
         self.itemslots[slot_num] = ItemSlot(self, 3, 4, -106)
 
@@ -213,9 +230,10 @@ class App(tk.Frame):
         slot_num = 9
         for r in range(0, 3):
             for c in range(0, 9):
-                self.itemslots[slot_num] = ItemSlot(self, r + startr, c, slot_num)
+                self.itemslots[slot_num] = ItemSlot(
+                    self, r + startr, c, slot_num)
                 slot_num += 1
-        
+
         # The equip row
         startr = 7
         slot_num = 0
@@ -237,36 +255,36 @@ class App(tk.Frame):
 
         # Create the save and load file location inputs
         self.text_save_location = tk.Entry(
-            self.input_frame, 
+            self.input_frame,
             textvariable=self.var_save_path)
         self.text_save_location.pack(fill=BOTH, expand=True)
         self.button_save_location = tk.Button(
-            self.input_frame, 
-            text="Set Save Location", 
+            self.input_frame,
+            text="Set Save Location",
             command=self.get_save_location)
         self.button_save_location.pack(fill=BOTH, expand=True)
 
         self.text_load_location = tk.Entry(
-            self.input_frame, 
+            self.input_frame,
             textvariable=self.var_load_path)
         self.text_load_location.pack(fill=BOTH, expand=True)
         self.button_load_location = tk.Button(
-            self.input_frame, 
-            text="Set Load Location", 
+            self.input_frame,
+            text="Set Load Location",
             command=self.get_load_location)
         self.button_load_location.pack(fill=BOTH, expand=True)
 
         # Create the save and load buttons
         self.button_save = tk.Button(
-            self.input_frame, 
-            text="Save to file", 
+            self.input_frame,
+            text="Save to file",
             command=self.save)
         # self.button_save.grid(row = 1, column=5, columnspan=3, sticky='nsew')
         self.button_save.pack(fill=BOTH, expand=True)
 
         self.button_load = tk.Button(
-            self.input_frame, 
-            text="Load from file", 
+            self.input_frame,
+            text="Load from file",
             command=self.load)
         # self.button_load.grid(row = 2, column=5, columnspan=3, sticky='nsew')
         self.button_load.pack(fill=BOTH, expand=True)
@@ -292,8 +310,8 @@ class App(tk.Frame):
         self.inventoryHandler.write_items(items, save_path)
 
     def load(self):
-        # TODO: Since this is modifying the item slots, we should probably 
-        # empty them all first (so we don't get remaining data from a 
+        # TODO: Since this is modifying the item slots, we should probably
+        # empty them all first (so we don't get remaining data from a
         # previously loaded file).
         for itemslot in self.itemslots:
             self.itemslots[itemslot].item = Item(itemslot)
@@ -326,12 +344,22 @@ class App(tk.Frame):
         self.text_load_location.delete(0, tk.END)
         self.text_load_location.insert(0, str(self.load_path.absolute()))
 
-root = tk.Tk()
-root.title("MC Inventory Editor")
-root.minsize(800, 600)
-# root.configure(background="#c6c6c6")
-# root.maxsize(800, 600)
-# TODO: This should probably not be a global
-itemSpriteHandler = ItemSpriteHandler()
-app = App(root)
-app.mainloop()
+
+itemSpriteHandler = None
+
+
+def main():
+    global itemSpriteHandler
+    root = tk.Tk()
+    root.title("MC Inventory Editor")
+    root.minsize(800, 600)
+    # root.configure(background="#c6c6c6")
+    # root.maxsize(800, 600)
+    # TODO: This should probably not be a global
+    itemSpriteHandler = ItemSpriteHandler()
+    app = App(root)
+    app.mainloop()
+
+
+if __name__ == '__main__':
+    main()
